de126a222e7ae52e12d8e7b5b83d63238914d886
Gupye <sbirjuk@gmail.com>
2021-07-15 20:42:20 +0300

    Second Try

--- main.py
+++ main.py
@@ -1,134 +1,125 @@
-from flask import Flask, render_template, request, session
+from flask.views import MethodView
+from flask import render_template, Flask, request, session
 import random
 
-app = Flask(__name__, template_folder='Templates/')
-app.config['SECRET_KEY'] = '467d4dc4eef096f79f88523dfd8fbe661a2c437c'
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '467d4dc4eef096f79f88522dfd8fbe661a1c437c'
 
 
-@app.route('/')
-def main():
-    if 'flag_ready' not in session:
-        session['flag_ready'] = False
-    if 'firtrue' not in session:
-        firtrue = 100
-        sectrue = 100
+class ShowUsers(MethodView):
 
-        session['firtrue'] = firtrue
-        session['sectrue'] = sectrue
+    def get(self):
+        if 'flag_ready' not in session:
+            session['flag_ready'] = False
+        if 'fir_true' not in session:
+            session['fir_true'] = 100
+            session['sec_true'] = 100
 
-    if 'history1' not in session:
-        session['history1'] = []
-        session['history2'] = []
-
-    history1 = session['history1']
-    history2 = session['history2']
-    if 'numhis' not in session:
-        session['numhis'] = []
-
-    return render_template('main.html', spirit=False, his1=history1, his2=history2, numhis=session['numhis'],
-                           ran=len(history2), messsage='Теперь введите двузначное число', firtrue=session['firtrue'],
-                           sectrue=session['sectrue'])
-
-
-@app.route('/ready', methods=['post'])
-def ready():
-    nums = []
-    session.permanent = False
-    if 'firtrue' not in session:
-        firtrue = 100
-        sectrue = 100
-
-        session['firtrue'] = firtrue
-        session['sectrue'] = sectrue
-
-    if 'history1' in session:
-
-        history1 = session['history1']
-        history2 = session['history2']
-
-    else:
-        history1 = []
-        history2 = []
-
-    if 'numhis' not in session:
-        session['numhis'] = []
-
-    if not session['flag_ready']:
-        for i in range(0, 2):
-            nums.append(random.randint(10, 99))
-        history1.append(nums[0])
-        history2.append(nums[1])
-
-    session['history1'] = history1
-    session['history2'] = history2
-
-    session['nums'] = nums
-
-    print(len(history2))
-
-    session['flag_ready'] = True
-    return render_template('main.html', spirit=True, nums=nums, his1=history1, his2=history2, numhis=session['numhis'],
-                           ran=len(history2), message='Теперь введите число', firtrue=session['firtrue'],
-                           sectrue=session['sectrue'])
-
-
-@app.route('/commit', methods=['post'])
-def commit():
-    print(request.form)
-    print(session)
-
-    if 'firtrue' not in session:
-        firtrue = 100
-        sectrue = 100
-
-        session['firtrue'] = firtrue
-        session['sectrue'] = sectrue
-
-    message = 'Теперь введите двузначное число'
-
-    if session['flag_ready'] == True and request.form['Number'] and len(request.form['Number']) == 2:
         if 'history1' not in session:
             session['history1'] = []
             session['history2'] = []
 
-        if 'numhis' in session:
+        if 'num_his' not in session:
+            session['num_his'] = []
 
-            numhis = session['numhis']
-            numhis.append(request.form['Number'])
-            session['numhis'] = numhis
-        else:
-            numhis = []
-            session['numhis'] = numhis.append(request.form['Number'])
-            print(session['numhis'])
+        return render_template('main.html', his1=session['history1'], his2=session['history2'],
+                               numhis=session['num_his'], ran=len(session['history2']),
+                               message='Теперь введите двузначное число', firtrue=session['fir_true'],
+                               sectrue=session['sec_true'])
 
-        for i in range(len(session['nums'])):
-            print('check', request.form['Number'], 'and', session['nums'][i])
-            if int(request.form['Number']) == int(session['nums'][i]):
-                if i == 0:
-                    session['firtrue'] += 1
-                else:
-                    session['sectrue'] += 1
-            else:
-                if i == 0:
-                    session['firtrue'] -= 1
-                else:
-                    session['sectrue'] -= 1
-        message = 'Загадайте новое число'
-        session['flag_ready'] = False
+    def check_num(self):
 
-    elif not session['flag_ready']:
-        message = 'Для начала загадайте число'
+        if 'fir_true' not in session:
+            fir_true = 100
+            sec_true = 100
 
-    elif not request.form['Number']:
+            session['fir_true'] = fir_true
+            session['sec_true'] = sec_true
+
         message = 'Теперь введите двузначное число'
+        spirit = True
+        if str(request.form['Number']).isdigit():
+            if session['flag_ready'] and request.form['Number'] and len(request.form['Number']) == 2 and \
+                    int(request.form['Number']) > 0:
+                spirit = False
+                if 'history1' not in session:
+                    session['history1'] = []
+                    session['history2'] = []
 
-    history1 = session['history1']
-    history2 = session['history2']
-    print(len(history2))
+                if 'num_his' in session:
 
-    return render_template('main.html', spirit=False, his1=history1, his2=history2, numhis=session['numhis'],
-                           ran=len(history2), message=message, firtrue=session['firtrue'],
-                           sectrue=session['sectrue'])
+                    num_his = session['num_his']
+                    num_his.append(request.form['Number'])
+                    session['num_his'] = num_his
+                else:
+                    num_his = []
+                    session['num)his'] = num_his.append(request.form['Number'])
 
-# if __name__ == "__main__":
-#     app.run(debug=True)
+                trues = [int(session['nums'][0]), int(session['nums'][1])]
+                if int(request.form['Number']) in trues:
+                    if trues.index(int(request.form['Number'])) == 0:
+                        session['fir_true'] += 1
+                        session['sec_true'] -= 1
+                    else:
+                        session['sec_true'] += 1
+                        session['fir_true'] -= 1
+                else:
+                    session['fir_true'] -= 1
+                    session['sec_true'] -= 1
+
+                message = 'Загадайте новое двузначное число'
+                session['flag_ready'] = False
+
+        elif not session['flag_ready']:
+            message = 'Для начала загадайте число'
+
+        return render_template('main.html', spirit=spirit, his1=session['history1'], his2=session['history2'],
+                               numhis=session['num_his'], nums=session['nums'], ran=len(session['history1']),
+                               message=message, firtrue=session['fir_true'], sectrue=session['sec_true'])
+
+    def make_rand_num(self):
+
+        session.permanent = False
+        if 'fir_true' not in session:
+            session['fir_true'] = 100
+            session['sec_true'] = 100
+
+        if 'history1' in session:
+            history1 = session['history1']
+            history2 = session['history2']
+
+        else:
+            history1 = []
+            history2 = []
+
+        if 'num_his' not in session:
+            session['num_his'] = []
+
+        if not session['flag_ready']:
+            nums = []
+            for i in range(0, 2):
+                nums.append(random.randint(10, 99))
+            history1.append(nums[0])
+            history2.append(nums[1])
+            session['nums'] = nums
+
+        session['history1'] = history1
+        session['history2'] = history2
+        session['flag_ready'] = True
+
+        return render_template('main.html', spirit=True, his1=session['history1'], his2=session['history2'],
+                               numhis=session['num_his'], nums=session['nums'], ran=len(history2),
+                               message='Теперь введите двузначное число', firtrue=session['fir_true'],
+                               sectrue=session['sec_true'])
+
+    def post(self):
+        if 'Number' in request.form:
+            return self.check_num()
+        else:
+            return self.make_rand_num()
+
+
+app.add_url_rule('/', view_func=ShowUsers.as_view('show_users'))
+
+app.run(debug=True)
---
+++ old_main.py
@@ -0,0 +1,136 @@
+import string
+
+from flask import Flask, render_template, request, session
+import random
+
+app = Flask(__name__, template_folder='Templates/')
+app.config['SECRET_KEY'] = '467d4dc4eef096f79f88523dfd8fbe661a2c437c'
+
+
+@app.route('/')
+def main():
+    if 'flag_ready' not in session:
+        session['flag_ready'] = False
+    if 'firtrue' not in session:
+        firtrue = 100
+        sectrue = 100
+
+        session['firtrue'] = firtrue
+        session['sectrue'] = sectrue
+
+    if 'history1' not in session:
+        session['history1'] = []
+        session['history2'] = []
+
+    history1 = session['history1']
+    history2 = session['history2']
+    if 'numhis' not in session:
+        session['numhis'] = []
+
+    return render_template('old_main.html', spirit=False, his1=history1, his2=history2, numhis=session['numhis'],
+                           ran=len(history2), message='Теперь введите двузначное число', firtrue=session['firtrue'],
+                           sectrue=session['sectrue'])
+
+
+@app.route('/ready', methods=['post'])
+def ready():
+    nums = []
+    session.permanent = False
+    if 'firtrue' not in session:
+        firtrue = 100
+        sectrue = 100
+
+        session['firtrue'] = firtrue
+        session['sectrue'] = sectrue
+
+    if 'history1' in session:
+
+        history1 = session['history1']
+        history2 = session['history2']
+
+    else:
+        history1 = []
+        history2 = []
+
+    if 'numhis' not in session:
+        session['numhis'] = []
+
+    if not session['flag_ready']:
+        for i in range(0, 2):
+            nums.append(random.randint(10, 99))
+        history1.append(nums[0])
+        history2.append(nums[1])
+
+    session['history1'] = history1
+    session['history2'] = history2
+
+    session['nums'] = nums
+
+    print(len(history2))
+
+    session['flag_ready'] = True
+    return render_template('old_main.html', spirit=True, nums=nums, his1=history1, his2=history2, numhis=session['numhis'],
+                           ran=len(history2), message='Теперь введите число', firtrue=session['firtrue'],
+                           sectrue=session['sectrue'])
+
+
+@app.route('/commit', methods=['post'])
+def commit():
+    print(request.form)
+    print(session)
+
+    if 'firtrue' not in session:
+        firtrue = 100
+        sectrue = 100
+
+        session['firtrue'] = firtrue
+        session['sectrue'] = sectrue
+
+    message = 'Теперь введите двузначное число'
+
+    if session['flag_ready'] == True and request.form['Number'] and len(request.form['Number']) == 2 and request.form['Number'] >0 and string.digits:
+        if 'history1' not in session:
+            session['history1'] = []
+            session['history2'] = []
+
+        if 'numhis' in session:
+
+            numhis = session['numhis']
+            numhis.append(request.form['Number'])
+            session['numhis'] = numhis
+        else:
+            numhis = []
+            session['numhis'] = numhis.append(request.form['Number'])
+            print(session['numhis'])
+
+        for i in range(len(session['nums'])):
+            print('check', request.form['Number'], 'and', session['nums'][i])
+            if int(request.form['Number']) == int(session['nums'][i]):
+                if i == 0:
+                    session['firtrue'] += 1
+                else:
+                    session['sectrue'] += 1
+            else:
+                if i == 0:
+                    session['firtrue'] -= 1
+                else:
+                    session['sectrue'] -= 1
+        message = 'Загадайте новое число'
+        session['flag_ready'] = False
+
+    elif not session['flag_ready']:
+        message = 'Для начала загадайте число'
+
+    elif not request.form['Number']:
+        message = 'Теперь введите двузначное число'
+
+    history1 = session['history1']
+    history2 = session['history2']
+    print(len(history2))
+
+    return render_template('old_main.html', spirit=False, his1=history1, his2=history2, numhis=session['numhis'],
+                           ran=len(history2), message=message, firtrue=session['firtrue'],
+                           sectrue=session['sectrue'])
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
